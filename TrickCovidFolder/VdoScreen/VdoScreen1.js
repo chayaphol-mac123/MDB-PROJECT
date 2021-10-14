a81af8289d5c61519f5dff9aa22a51529f7f78ca
Songkeit <6210742083@student.tu.ac.th>
2021-10-14 17:49:57 +0700

    youtube_edit

--- TrickCovidFolder/VdoScreen/VdoScreen1.js
+++ TrickCovidFolder/VdoScreen/VdoScreen1.js
@@ -1,12 +1,39 @@
-import React, {useState} from 'react';
-import {View, Text, Button, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import React from 'react';
+import {View, Text, StyleSheet, ScrollView,} from 'react-native';
+import YoutubePlayer from 'react-native-youtube-iframe'
 
 const VdoScreen = ({navigation}) => {
     return(
-        <View style={styles.header}>
-            <Text>Vdo Screen</Text>
+        <ScrollView>
+            <View>
+                <Text>IU jogggggg</Text>
+            <YoutubePlayer 
+            height={300}
+            play={true}
+            videoId={'D1PvIWdJ8xo'}
+            />
+            <Text>asasas</Text>
+            <YoutubePlayer 
+            height={300}
+            play={true}
+            videoId={'1DXQpPr0UZs'}
+            />
+            <Text>IU </Text>
+            <YoutubePlayer 
+            height={300}
+            play={true}
+            videoId={'CxoJO6oEDSs'}
+            />
+             <YoutubePlayer 
+            height={300}
+            play={true}
+            videoId={'Z1eFppbx8oc'}
+            />
         </View>
+        </ScrollView>
     )
+        
+        
 }
 
 export default VdoScreen;
